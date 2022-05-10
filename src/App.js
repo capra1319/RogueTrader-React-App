f3732c5b9c246caf75e061634ae62485d4b9b631
Capra <arb.95@outlook.com>
2022-05-10 21:48:52 +1000

    fixed up buttons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import codecademyLogo from './assets/codecademy-logo.png'
 import './App.css';
 import { getByDisplayValue } from '@testing-library/react';
 
@@ -21,24 +22,54 @@ function App() {
           Link to the React site for some starting information!
         </a>
       </header>
-      <div className="Main-Content" style={{fontWeight:"500"}}>
+      <div 
+        className="Main-Content" 
+        style={{fontWeight:"500"}}
+        data-aos="fade-down"
+        data-aos-easing="linear"
+        data-aos-duration="1500"
+      >
         <p>
           Or choose one of the Following other options below, because Codecademy has some pretty crazy course packages!
         </p>
-        <a
-          href="https://www.codecademy.com/learn/paths/front-end-engineer-career-path"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button 
-            className="button"
-          >
-            <span>
-                The Magic of codecademy!
-              <img src={logo} className="App-logo-small" alt="logo" />
-            </span>
-          </button>
-        </a>
+        <img 
+          src={codecademyLogo} 
+          className="logo" 
+          alt="logo"
+          style={{maxheight:"300px", paddingTop:"50px", paddingBottom:"50px"}}
+        />
+        <div>
+          <a
+            href="https://www.codecademy.com/learn/paths/front-end-engineer-career-path"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            <button 
+              className="button"
+              style={{Width:"40vw"}}
+              >
+              <span>
+                Front End Developer Course
+                <img src={logo} className="App-logo-small" alt="logo" />
+              </span>
+            </button>
+          </a>
+          <a
+            href="https://www.codecademy.com/learn/paths/full-stack-engineer-career-path"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            <button 
+              className="button"
+              style={{Width:"40vw"}}
+              >
+              <span>
+                Full Stack Engineer Course
+                <img src={logo} className="App-logo-small" alt="logo" />
+              </span>
+            </button>
+          </a>
+        </div>
       </div>
     </div >
   );

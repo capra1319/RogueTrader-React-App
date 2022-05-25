668a984cf198f5414a1d758bcfa4c70e64582e0f
Capra <arb.95@outlook.com>
2022-05-25 19:37:20 +1000

    slight changes to wording and styling

--- src/assets/content_01.js
+++ src/assets/content_01.js
@@ -5,23 +5,23 @@ import codecademyLogo from './codecademy-logo.png'
 function content_01() {
   return (
       <div 
-        className="Main-Content_01" 
-        style={{fontWeight:"500", height:"80vh", marginTop:"20vh"}}
+        className="Main-Content" 
+        style={{fontWeight:"500", height:"80vh", paddingTop:"20vh"}}
         data-aos="fade-right"
         data-aos-anchor-placement="top-center"
         data-aos-duration="1000"
         // data-aos-offset="0"
       >
-        <p>
-          Or choose one of the Following other options below, because Codecademy has some pretty crazy course packages!
-        </p>
+        <p>I've decided to use the learning meterial from codecademy.com, due to their flexability and learn when you can structure through videos and virtual online text editor envirouments.</p>
+        <p>They users can start smallre courses that focus on a particular skill or users can start a full job course which bundles all the necessary skills required to start in a specific career path.</p>
         <img 
           src={codecademyLogo} 
           className="logo" 
-          alt="logo"
-          style={{maxheight:"300px", paddingTop:"50px", paddingBottom:"50px"}}
+          alt="logo icon from codecademy.com"
+          style={{maxWidth:"50vw", paddingTop:"50px", paddingBottom:"50px"}}
         />
-        <div>
+        <p>These two course packages are my primary choices</p>
+        <div className="2-col-container">
           <a
             href="https://www.codecademy.com/learn/paths/front-end-engineer-career-path"
             target="_blank"
